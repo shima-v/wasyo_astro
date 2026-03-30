0a076862a76cb92a4d3dc8d82bc9e85cbf2767be
shima hiromasa <mahiro.speedhand@gmail.com>
2026-03-30 20:56:48 +0900

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // site: 'https://[ユーザー名].github.io',
+  // base: '/[リポジトリ名]',
+  // ※後で独自ドメインにする場合はここを修正しますが、一旦はこのままでも動く設定が多いです。
+  // GitHub Pagesのサブディレクトリで動かす場合は base: '/リポジトリ名' が必要になります。
+  site: 'https://shima-v.github.io',
+  base: 'wasyo_astro',
+});
