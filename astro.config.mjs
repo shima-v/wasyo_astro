9ddeb990ac957831874aa62ace5dbb1ce2103b87
shima hiromasa <mahiro.speedhand@gmail.com>
2026-04-07 16:39:31 +0900

    config: update astro.config for custom domain transition
    Update site URL to www.wwwasyo.com
    
    Reset base path from '/wasyo_astro/' to '/'
    
    Prepare for deployment to root domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://[ユーザー名].github.io',
-  // base: '/[リポジトリ名]',
-  // ※後で独自ドメインにする場合はここを修正しますが、一旦はこのままでも動く設定が多いです。
-  // GitHub Pagesのサブディレクトリで動かす場合は base: '/リポジトリ名' が必要になります。
-  site: 'https://shima-v.github.io',
-  base: 'wasyo_astro',
+  site: 'https://www.wwwasyo.com', // 取得したドメイン
+  base: '/',                       // サブディレクトリ（wasyo_astro）を空にする
 });
